955814d4986e120606a3ef12c562df46ba731ce6
José Jorge <jorgesouza621@gmail.com>
2024-06-18 15:32:08 -0300

    Finalizado container de informações da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,66 @@
-import { HomeContainer } from './styles'
+import { HomeContainer, InfoContainer, IntroContainer } from './styles'
+import coffeImage from '../../assets/coffeeImage.svg'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import { defaultTheme } from '../../styles/themes/default'
 
 export function Home() {
-  return <HomeContainer> HOME</HomeContainer>
+  return (
+    <div>
+      <HomeContainer>
+        <div className="general">
+          <IntroContainer>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+
+            <InfoContainer>
+              <div className="infos">
+                <ShoppingCart
+                  weight="fill"
+                  size={16}
+                  style={{ backgroundColor: defaultTheme['yellow-dark'] }}
+                  color={defaultTheme.background}
+                  className="svgs"
+                />
+                Compra simples e segura
+              </div>
+
+              <div className="infos">
+                <Package
+                  weight="fill"
+                  size={16}
+                  style={{ backgroundColor: defaultTheme.text }}
+                  color={defaultTheme.background}
+                />
+                Embalagem mantém o café intacto
+              </div>
+
+              <div className="infos">
+                <Timer
+                  weight="fill"
+                  size={16}
+                  style={{ backgroundColor: defaultTheme.yellow }}
+                  color={defaultTheme.background}
+                />
+                Entrega rápida e rastreada
+              </div>
+
+              <div className="infos">
+                <Coffee
+                  weight="fill"
+                  size={16}
+                  style={{ backgroundColor: defaultTheme.purple }}
+                  color={defaultTheme.background}
+                />
+                O café chega fresquinho até você
+              </div>
+            </InfoContainer>
+          </IntroContainer>
+          <img src={coffeImage} alt="" />
+        </div>
+      </HomeContainer>
+    </div>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,58 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
-  padding-top: 104px;
+  background-image: url('src/assets/Background.svg');
+  background-size: cover;
+  height: 544px;
+
+  .general {
+    display: flex;
+    max-width: 1120px;
+    margin: 0 auto;
+  }
+  img {
+    width: 476px;
+    height: 360px;
+  }
+`
+
+export const IntroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  h1 {
+    font-family: 'Baloo 2';
+    font-weight: 800;
+    font-size: 48px;
+    margin-right: 56px;
+  }
+
+  p {
+    font-family: 'Roboto';
+    font-size: 20px;
+    font-weight: 400;
+    color: ${(props) => props.theme.subtitle};
+  }
+`
+export const InfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px 40px;
+  margin-top: 66px;
+  font-size: 16px;
+  color: ${(props) => props.theme.text};
+
+  svg {
+    width: 32px;
+    height: 32px;
+    padding: 8px;
+    border-radius: 50%;
+  }
+
+  .infos {
+    display: flex;
+    align-items: center;
+    gap: 5.5px;
+  }
 `
