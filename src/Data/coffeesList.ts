76e0d84be413039258eb435af1b0167eb8206b10
José Jorge <jorgesouza621@gmail.com>
2024-07-04 21:49:48 -0300

    Início da criação do arquivo de objetos coffeeFlavors

---
+++ src/Data/coffeesList.ts
@@ -0,0 +1,19 @@
+interface CoffeeFlavorsTypes {
+  id: number
+  name: string
+  typeOfCoffee: string[]
+  description: string
+  price: number
+  picture: string
+}
+
+export const coffeeFlavors: CoffeeFlavorsTypes[] = [
+  {
+    id: 0,
+    name: 'Expresso Tradicional',
+    typeOfCoffee: ['Tradicional'],
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    price: 9.9,
+    picture: '/src/assets/coffees/expresso-tradicional.svg',
+  },
+]
