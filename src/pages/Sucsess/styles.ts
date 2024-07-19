67160c3aeb4a95a16dae110683d5aa7fed700626
José Jorge <jorgesouza621@gmail.com>
2024-07-19 14:24:14 -0300

    🧹 Limpeza de código

--- src/pages/Sucsess/styles.ts
+++ src/pages/Sucsess/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.div`
   grid-row-gap: 28px;
   grid-template-areas:
     'confirmation .'
-    'bg picture';
+    'infos picture';
 
   max-width: 1120px;
   margin: 80px auto;
@@ -16,8 +16,8 @@ export const Container = styled.div`
     grid-area: confirmation;
   }
 
-  #background {
-    grid-area: bg;
+  #infos {
+    grid-area: infos;
   }
 
   #picture {
@@ -40,24 +40,14 @@ export const Confirmation = styled.div`
   }
 `
 
-export const BackgroundDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 526px;
-  height: 270px;
-  padding: 1px;
-  border-radius: 6px 36px;
-  background-image: linear-gradient(#dbac2c, #8047f8);
-`
 export const Infos = styled.div`
   display: flex;
   flex-direction: column;
   gap: 32px;
-
-  width: 525px;
-  height: 269px;
-  padding: 39px;
+  width: 526px;
+  height: 270px;
+  padding: 40px;
+  position: relative;
 
   border-radius: 6px 36px;
   background-color: ${(props) => props.theme.background};
@@ -68,6 +58,15 @@ export const Infos = styled.div`
     height: 32px;
     padding: 8px;
     border-radius: 50%;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: -1px;
+    border-radius: 7px 37px;
+    z-index: -1;
+    background-image: linear-gradient(#dbac2c, #8047f8);
   }
 `
 
