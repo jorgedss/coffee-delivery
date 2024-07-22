b7df63f64473664bf97aa3fe56b740b037ec0597
José Jorge <jorgesouza621@gmail.com>
2024-07-22 11:05:17 -0300

    Separação de componentes da Home

---
+++ src/pages/Home/IntroContainer/index.tsx
@@ -0,0 +1,57 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import { InfoContainer, IntroContainerStyle } from './styles'
+import { defaultTheme } from '../../../styles/themes/default'
+
+export function IntroContainer() {
+  return (
+    <IntroContainerStyle>
+      <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+      <p>
+        Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+      </p>
+
+      <InfoContainer>
+        <div className="infos">
+          <ShoppingCart
+            weight="fill"
+            size={16}
+            style={{ backgroundColor: defaultTheme['yellow-dark'] }}
+            color={defaultTheme.background}
+            className="svgs"
+          />
+          Compra simples e segura
+        </div>
+
+        <div className="infos">
+          <Package
+            weight="fill"
+            size={16}
+            style={{ backgroundColor: defaultTheme.text }}
+            color={defaultTheme.background}
+          />
+          Embalagem mantém o café intacto
+        </div>
+
+        <div className="infos">
+          <Timer
+            weight="fill"
+            size={16}
+            style={{ backgroundColor: defaultTheme.yellow }}
+            color={defaultTheme.background}
+          />
+          Entrega rápida e rastreada
+        </div>
+
+        <div className="infos">
+          <Coffee
+            weight="fill"
+            size={16}
+            style={{ backgroundColor: defaultTheme.purple }}
+            color={defaultTheme.background}
+          />
+          O café chega fresquinho até você
+        </div>
+      </InfoContainer>
+    </IntroContainerStyle>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,72 +1,18 @@
-import {
-  CoffeesList,
-  HomeContainer,
-  InfoContainer,
-  IntroContainer,
-  ListContainer,
-} from './styles'
+import { CoffeesList, HomeContainer, ListContainer } from './styles'
 import coffeImage from '../../assets/coffeeImage.svg'
-import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
-import { defaultTheme } from '../../styles/themes/default'
+
 import { CoffeeCard } from './CoffeesCards'
 
 import { coffeeFlavors } from '../../Data/coffeesList'
+import { IntroContainer } from './IntroContainer'
 
 export function Home() {
   return (
     <div>
       <HomeContainer>
         <div className="general">
-          <IntroContainer>
-            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-            <p>
-              Com o Coffee Delivery você recebe seu café onde estiver, a
-              qualquer hora
-            </p>
+          <IntroContainer />
 
-            <InfoContainer>
-              <div className="infos">
-                <ShoppingCart
-                  weight="fill"
-                  size={16}
-                  style={{ backgroundColor: defaultTheme['yellow-dark'] }}
-                  color={defaultTheme.background}
-                  className="svgs"
-                />
-                Compra simples e segura
-              </div>
-
-              <div className="infos">
-                <Package
-                  weight="fill"
-                  size={16}
-                  style={{ backgroundColor: defaultTheme.text }}
-                  color={defaultTheme.background}
-                />
-                Embalagem mantém o café intacto
-              </div>
-
-              <div className="infos">
-                <Timer
-                  weight="fill"
-                  size={16}
-                  style={{ backgroundColor: defaultTheme.yellow }}
-                  color={defaultTheme.background}
-                />
-                Entrega rápida e rastreada
-              </div>
-
-              <div className="infos">
-                <Coffee
-                  weight="fill"
-                  size={16}
-                  style={{ backgroundColor: defaultTheme.purple }}
-                  color={defaultTheme.background}
-                />
-                O café chega fresquinho até você
-              </div>
-            </InfoContainer>
-          </IntroContainer>
           <img src={coffeImage} alt="" />
         </div>
       </HomeContainer>
